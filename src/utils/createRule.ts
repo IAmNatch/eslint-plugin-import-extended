5ba4018abf57834caa7ed12f8cad6b19558a8fcb
iAmNatch <shaner@snappe.io>
2022-07-01 14:48:23 -0400

    fix: rule name was incorrectly configured

--- src/utils/createRule.ts
+++ src/utils/createRule.ts
@@ -2,5 +2,5 @@ import {ESLintUtils} from "@typescript-eslint/utils";
 
 export const createRule = ESLintUtils.RuleCreator(
     (ruleName) =>
-        `https://github.com/IAmNatch/eslint-plugin-import-extended${ruleName}`
+        `https://github.com/IAmNatch/eslint-plugin-import-extended#${ruleName}`
 );
